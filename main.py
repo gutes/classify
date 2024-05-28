f73a9f6e5c3bfebfda584e70cfe79120e9e673ac
Ezequiel Gutesman <egutesman@gmail.com>
2024-05-28 14:56:25 -0300

    - Fixed routing problem due to precedence of static folder - fixed float type for x, y in Image(BaseModel) which caused '422 Unprocessable Entitiy" status code

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ from fastapi.staticfiles import StaticFiles
 class Image(BaseModel):
    category: str
    imageId: str
-   x: int
-   y: int
+   x: float
+   y: float
 
 class Classification(BaseModel):
     reasoning: str
     categoryNames: List[str]
     imagePositions: List[Image]
 
-
 app = FastAPI()
+router = APIRouter()
+
+@router.post("/save")
+async def root(classification : Classification):
+  return classification
+
+# Precedence matters while adding mounting and routing handlers
+app.include_router(router)
 app.mount('/', StaticFiles(directory='static', html=True))
 app.mount('/img', StaticFiles(directory='static/img', html=False))
-
-router = APIRouter()
-@router.post("/save")
-async def root(request):
-  return "hola"
-
-app.include_router(router)
-
